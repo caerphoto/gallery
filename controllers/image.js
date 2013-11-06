2536168da7d0e1d55735ffc6a246196287c9fd9f
Andy <andrew.farrell@phgroup.com>
2013-11-06 11:30:16 +0000

    Add error checking in case of image retrieval issues.

--- controllers/image.js
+++ controllers/image.js
@@ -8,6 +8,10 @@ exports.fetch = function( req, res ) {
     var filename = req.params.filename;
 
     db.hgetall( utils.getImageKey( filename ), function( err, image ) {
+        if ( err ) {
+            return res.send( 500, "Error retrieving image metadata." );
+        }
+
         res.set( "Content-Type", image.type );
         res.set( "Cache-Control", "public, max-age=604800" ); // 1 week
         res.sendfile( utils.getImagePath( filename ) );
@@ -18,6 +22,10 @@ exports.fetchThumb = function( req, res ) {
     var filename = req.params.filename;
 
     db.hgetall( utils.getImageKey( filename ), function( err, image ) {
+        if ( err ) {
+            return res.send( 500, "Error retrieving thumbnail metadata." );
+        }
+
         res.set( "Content-Type", image.type );
         res.set( "Cache-Control", "public, max-age=604800" ); // 1 week
         res.sendfile( utils.getImageThumbPath( filename ) );
