0a664944d1759290872143138000f0ebc483c973
Andy <andrew.farrell@phgroup.com>
2013-11-06 11:44:44 +0000

    Improve error checking and return more accurate errors.

--- controllers/image.js
+++ controllers/image.js
@@ -12,6 +12,10 @@ exports.fetch = function( req, res ) {
             return res.send( 500, "Error retrieving image metadata." );
         }
 
+        if ( !image ) {
+            return res.send( 404, "Image not found." );
+        }
+
         res.set( "Content-Type", image.type );
         res.set( "Cache-Control", "public, max-age=604800" ); // 1 week
         res.sendfile( utils.getImagePath( filename ) );
@@ -24,6 +28,10 @@ exports.fetchThumb = function( req, res ) {
     db.hgetall( utils.getImageKey( filename ), function( err, image ) {
         if ( err ) {
             return res.send( 500, "Error retrieving thumbnail metadata." );
+        }
+
+        if ( !image ) {
+            return res.send( 404, "Thumbnail not found." );
         }
 
         res.set( "Content-Type", image.type );
